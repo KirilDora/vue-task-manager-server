9c9a8a1b0ba003cc62500c9b10825c4c43bab69f
KirilDora <kirildora02@gmail.com>
2025-04-19 13:42:03 +0300

    config fixed

--- config/index.js
+++ config/index.js
@@ -1,11 +1,6 @@
 const OPTIONS = {};
 
 OPTIONS.FILEPATH = '../';
-// OPTIONS.FILEPATH = '../front-pulsar-release/dist';
-// OPTIONS.FILEPATH = '../encashment/dist/encashment';
-// OPTIONS.FILEPATH = '../a-bank.com.ua/dist';
-// OPTIONS.FILEPATH = '../limit';
-OPTIONS.FILEPATH = '../transfers/build';
 
 // OPTIONS.FILEPATH = '../shortRequest/dist';
 
